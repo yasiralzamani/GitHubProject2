5dd462ae7a891b8657e4f72b96d364f744f2d481
yasiralzamani <yaser.alzamane@yahoo.com>
2019-06-13 19:56:56 -0400

    first one

--- src/hi/test.java
+++ src/hi/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("hello world");
+		System.out.println("test one ");
 	}
 
 }
